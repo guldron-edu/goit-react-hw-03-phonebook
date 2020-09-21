13757bdcd1743e30a402a00790776e17f2f2ba07
guldron-edu <guldron.edu@gmail.com>
2020-09-21 11:35:20 +0300

    Updates

---
+++ precache-manifest.5e4a85e353607dfa3c0af79e6d3f10d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41649ff214e087cf974878399b7aa430",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "080a5e05d173b40ca5c7",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.07c8138a.chunk.css"
+  },
+  {
+    "revision": "19c6c43fdbb4121e4897",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c317ad48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c317ad48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080a5e05d173b40ca5c7",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.d32a5f55.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
